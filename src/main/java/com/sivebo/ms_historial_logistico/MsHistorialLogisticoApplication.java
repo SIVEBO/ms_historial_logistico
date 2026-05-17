df554215424d02e85483da5ed4cea57ab897ed08
daemunozr <dae.munozr@duocuc.cl>
2026-05-17 10:57:19 -0400

    primer commit

---
+++ src/main/java/com/sivebo/ms_historial_logistico/MsHistorialLogisticoApplication.java
@@ -0,0 +1,13 @@
+package com.sivebo.ms_historial_logistico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHistorialLogisticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHistorialLogisticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sivebo/ms_historial_logistico/MsHistorialLogisticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivebo.ms_historial_logistico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHistorialLogisticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
